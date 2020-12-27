d220ebeecb58b3d3b2731839c0efa3457954e0db
nemo369 <nafrenkel@gmail.com>
2020-12-27 17:47:40 +0200

    make gernic transiet get post

--- wp-content/themes/canaan/framework/front_and_back/general.php
+++ wp-content/themes/canaan/framework/front_and_back/general.php
@@ -11,3 +11,23 @@ include_once(dirname(__FILE__).'/ajax.php');
 function canaan_static($root = ''){
 	return get_template_directory_uri().'/static' .($root ? '/'.$root : ''); 
 }
+function get_site_static($root = '')
+{
+	return get_template_directory_uri() . '/static' . ($root ? '/' . $root : '');
+}
+
+function get_posts_transiet($args, $transient, $time = MONTH_IN_SECONDS)
+{
+	// Do we have this information in our transients already?
+	$transient = get_transient($transient);
+	// Yep!  Just return it and we're done.
+	if (!empty($transient)) {
+		// The function will return here every time after the first time it is run, until the transient expires.
+		return $transient;
+		// Nope!  We gotta make a call.
+	} else {
+		$_posts = get_posts($args);
+		set_transient($transient, $_posts, $time);
+		return $_posts;
+	}
+}
