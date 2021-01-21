031a51e14d1acd72702c46aca30c594811dca75c
nemo369 <nafrenkel@gmail.com>
2021-01-21 16:55:12 +0200

    use composer for carbon fields

--- wp-content/themes/canaan/functions.php
+++ wp-content/themes/canaan/functions.php
@@ -4,8 +4,6 @@ if(!class_exists('canaan_conf')){
     die('wrong instaliton');
 }
  
- 
-include_once(dirname(__FILE__).'/wpack.php');
 include_once(dirname(__FILE__).'/theme-functions.php');
 include_once(dirname(__FILE__).'/framework/framework.php');
 include_once(dirname(__FILE__).'/post_types/register_post_types.php');
