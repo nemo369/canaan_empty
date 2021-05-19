d0e8bbcb2248bf328041a512fdc039ad9fdc53f7
naaman1 <nafrenkel@gmail.com>
2021-05-19 08:57:43 +0300

    BLOCK WP REST API FRON NOT LOGED USER

--- wp-content/themes/canaan/theme-functions.php
+++ wp-content/themes/canaan/theme-functions.php
@@ -61,6 +61,17 @@ remove_action('wp_head', 'wp_generator');
 remove_action('admin_notices', 'update_nag', 3);
 remove_action('network_admin_notices', 'update_nag', 3);
 
+// BLOCK WP REST API FRON NOT LOGED USER 
+// add_filter('rest_authentication_errors', function ($result) {
+
+//     if (!empty($result)) {
+//         return $result;
+//     }
+//     if (!is_user_logged_in()) {
+//         return new WP_Error('rest_not_logged_in', 'You are not currently logged in.', array('status' => 401));
+//     }
+//     return $result;
+// });
 
 
 /*
